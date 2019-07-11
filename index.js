380e9114d6fe7bdd876b6348e18765818bd7c7d7
asultan00 <asultan00@stuy.edu>
2019-07-11 16:07:24 -0400

    added index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+
+//Starting Express.js app
+const app = express();
+app.listen('99', () => {
+    console.log('Server started on port 99')
+});
+
+//Loading in default HTML page
+app.get('/', (req, res) => {
+    console.log("Loading index.html...")
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
