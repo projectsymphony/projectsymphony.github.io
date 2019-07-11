b6db767e70c776651ac82a5d2770769ba45ab065
asultan00 <asultan00@stuy.edu>
2019-07-11 16:56:52 -0400

    converted html to ejs

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ app.listen('99', () => {
     console.log('Server started on port 99')
 });
 
+app.set('view engine', 'ejs')
+
 app.use('/static', express.static('static'))
 
 //Loading in default HTML page
 app.get('/', (req, res) => {
-    console.log("im philip");
-    res.sendFile(path.join(__dirname + '/index.html'));
+    //console.log("im philip");
+    //res.sendFile(path.join(__dirname + '/index.html'));
+    res.render('index.ejs');
 });
