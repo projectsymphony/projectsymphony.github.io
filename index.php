ac819778a76fcde38fe6071374642bcd219d81b6
JosephYusufov <jyusufov00@stuy.edu>
2019-07-11 20:54:44 -0400

    website is working...

--- index.php
+++ index.php
@@ -11,34 +11,22 @@
         <h1>Symphony</h1>
         <h3>All your events, in one place.</h3>
         <br>
-        <br>
-        <br>
+        <div class="navbar"> <h3>Bruh</h3> </div>
         <?php
         $sql = "SELECT * FROM post";
         $result = mysqli_query($conn, $sql);
         if($result -> num_rows > 0){
-                // echo " <table> ";
-                // echo "     <tr> ";
-                // echo "         <th>Post ID</th> ";
-                // echo "         <th>Author</th> ";
-                // echo "         <th>Date</th> ";
-                // echo "         <th>Content</th> ";
-                // echo "     </tr> ";
+            echo "<div class='event-box-container'>";
                 while ($rowArray = mysqli_fetch_array($result, MYSQLI_ASSOC)){
                     echo "     <div class='event-block'>";
-                    echo "         <h4>" . $rowArray[content] . "</h4>";
-                    echo "         <h5>" . $rowArray[date] . " " . $rowArray[author] . "</h5>";
+                    echo "         <h4>" . $rowArray[title] . "</h4>";
+                    echo "         <h5>" . $rowArray[location] . " " . $rowArray[date] . " " . $rowArray[author] . "</h5>";
+                    echo "         <p>" . $rowArray[description] ;
                     echo " <br> <hr> <br> ";
                     echo "     </div>";
                     echo "     <br>";
-                    
-                    // echo "     <tr> ";
-                    // echo "         <td>" . $rowArray[post_id] . "</td> ";
-                    // echo "         <td>" . $rowArray[author] . "</td> ";
-                    // echo "         <td>" . $rowArray[date] . "</td> ";
-                    // echo "         <td>" . $rowArray[content] . "</td> ";
-                    // echo "     </tr> ";
                 }
+            echo "</div>";
         } else {
             die();
         }
