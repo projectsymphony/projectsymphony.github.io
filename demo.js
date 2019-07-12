bea61720184c020a742986d803192937d7b7fb81
asultan00 <asultan00@stuy.edu>
2019-07-11 20:53:59 -0400

    added reader

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
-const express = require('express');
-const http = require('http');
+const CSVtoJSON = require('csvtojson');
+const fs = require('fs');
 
 var con = mysql.createConnection({
     host: "35.231.65.224",
@@ -9,28 +9,40 @@ var con = mysql.createConnection({
     database: "Symphony"
 });
 
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-});
+/*
+
+CSVtoJSON.fromFile(process.argv[2]).then(grabbed => {
+    for (var i = 0; i < grabbed.length; i++) {
+        console.log(grabbed[i].title);
+}
+
+*/
 
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
-    for (var i = 0; var)
-    var sql = "INSERT INTO post (author, date, content) VALUES ('Pogchamp', 'today', 'poggers');";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Inserted successfully!");
-    });
-    var sql = "INSERT INTO post (author, date, content) VALUES ('Gamer helmet', 'today', 'gamer socks');";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Inserted successfully!");
-    });
-    var sql = "INSERT INTO post (author, date, content) VALUES ('son qbert', 'today', 'YEAHAHHHHHH');";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("Inserted successfully!");
+    CSVtoJSON().fromFile(process.argv[2]).then(grabbed => {
+        for (var i = 0; i < grabbed.length; i++) {
+            var sql = "INSERT INTO post (author, date, title, description, tags, location) VALUES (";
+            var date = "9/9/1999";
+            sql+= "'" + grabbed[i].author + "',";
+            sql+= "'" + date + "',";
+            sql+= "'" + grabbed[i].title + "',";
+            sql+= "'" + grabbed[i].description + "',";
+            sql+= "'" + grabbed[i].tags + "',";
+            sql+= "'" + grabbed[i].location + "'";
+            sql+= ");";
+            console.log(sql);
+            con.query(sql, function(err, result) {
+                if (err) throw err;
+                console.log("Inserted successfully!");
+            });
+        };
     });
 });
+    /*var sql = "INSERT INTO post (author, date, content) VALUES ('Pogchamp', 'today', 'poggers');";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Inserted successfully!");
+}   );
+    */
